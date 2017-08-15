b9120ad86300c012aff0e1c0ab21e1eb25133a22
Alberto Cubeddu <acubeddu87@gmail.com>
2017-08-16 09:34:44 +1000

    Update groupByBiggest.sql

--- mysqlSnippets/commonQuery/groupByBiggest.sql
+++ mysqlSnippets/commonQuery/groupByBiggest.sql
@@ -1,7 +1,7 @@
 #Match all the line with a smallest < bigger number and check the left joined table to see that no bigger date has found.
 
 SELECT s.*
-FROM `group_table` o                    # 's' from 'smallest extract in group'
+FROM `group_table` s                    # 's' from 'smallest extract in group'
   LEFT JOIN `group_table` b             # 'b' from 'bigger extract in group'
-      ON o.id = b.id AND o.number < b.number
+      ON s.id = b.id AND s.number < b.number
 WHERE b.number is NULL                 # bigger date not found
